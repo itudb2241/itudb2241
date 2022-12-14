643c602bdbf3040af27c9358a68b1f2c6df17568
Mo Chahadeh <applednd2002@gmail.com>
2022-12-14 12:52:38 +0300

    Added players page with dropdown of players

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import sqlite3
+
 
 app = Flask(__name__)
 
@@ -10,6 +12,16 @@ app.config.update(
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/players')
+def players():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    players = cursor.execute('SELECT playerId, firstName, lastName FROM Master WHERE playerId NOT NULL').fetchall()
+    print(players)
+    connection.close()
+
+    return render_template('players.html', players=players)
 
 
 if __name__ == '__main__':
